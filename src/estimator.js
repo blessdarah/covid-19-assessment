e31b140b13254c5677b5a17fefc47444a0796c13
blessdarah <blessdarahuba@gmail.com>
2020-04-09 09:38:29 +0100

    make future cases dynamic as per periods provided from data

--- src/estimator.js
+++ src/estimator.js
@@ -17,47 +17,46 @@
   }
 */
 
-
 function estimateFutureCases(data, currentInfectedCases) {
-  let factor;
-  switch (data.periodType) {
-    // 7 days * # 28 weeks
-    case 'weeks':
-      factor = Math.floor((7 * data.timeToElapse) / 3);
-      break;
-    case 'months':
-      // 30 days * 28 months
-      factor = Math.floor((30 * data.timeToElapse) / 3);
-      break;
-    default:
-      factor = Math.floor(data.timeToElapse / 3);
-      break;
-  }
+    let factor = null;
+    switch (data.periodType) {
+        // 7 days * # 28 weeks
+        case 'weeks':
+            factor = Math.floor((7 * data.timeToElapse) / 3);
+            break;
+        case 'months':
+            // 30 days * 28 months
+            factor = Math.floor((30 * data.timeToElapse) / 3);
+            break;
+        default:
+            factor = Math.floor(data.timeToElapse / 3);
+            break;
+    }
 
-  return currentInfectedCases * (2 ** factor);
+    return currentInfectedCases * 2 ** factor;
 }
 
 const covid19ImpactEstimator = (data) => {
-  const impact = {};
-  const severeImpact = {};
-  // reported cases
-  impact.currentlyInfected = data.reportedCases * 10;
-  severeImpact.currentlyInfected = data.reportedCases * 50;
-  // Estimate cases for the next 28 days on impact
-  impact.infectionsByRequestedTime = estimateFutureCases(
-    data,
-    impact.currentlyInfected
-  );
-  // Estimate cases for the next 28 days on severe impact
-  severeImpact.infectionsByRequestedTime = estimateFutureCases(
-    data,
-    severeImpact.currentlyInfected
-  );
-  return {
-    data,
-    impact,
-    severeImpact
-  };
+    const impact = {};
+    const severeImpact = {};
+    // reported cases
+    impact.currentlyInfected = data.reportedCases * 10;
+    severeImpact.currentlyInfected = data.reportedCases * 50;
+    // Estimate cases for the next 28 days on impact
+    impact.infectionsByRequestedTime = estimateFutureCases(
+        data,
+        impact.currentlyInfected
+    );
+    // Estimate cases for the next 28 days on severe impact
+    severeImpact.infectionsByRequestedTime = estimateFutureCases(
+        data,
+        severeImpact.currentlyInfected
+    );
+    return {
+        data,
+        impact,
+        severeImpact
+    };
 };
 
 export default covid19ImpactEstimator;
