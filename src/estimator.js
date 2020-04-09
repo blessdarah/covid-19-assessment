a36ef279433c42c534a58a3389c042aea0692507
blessdarah <blessdarahuba@gmail.com>
2020-04-09 05:47:13 +0100

    Fix eslint errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,34 +1,44 @@
-const covid19ImpactEstimator = (data) => {
-    const impact = {};
-    const severeImpact = {};
-
-    //  reported cases 
-    impact.currentlyInfected = data.reportedCases * 10;
-    severeImpact.currentlyInfected = data.reportedCases * 50;
-
-    // Estimate cases for the next 28 days on impact 
-    impact.infectionsByRequestedTime = estimateFutureCases(data.periodType, impact.currentlyInfected);
-
-    // Estimate cases for the next 28 days on severe impact 
-    severeImpact.infectionsByRequestedTime = estimateFutureCases(data.periodType, severeImpact.currentlyInfected);
-
-    return { data, impact, severeImpact };
-};
-
+/* eslint-disable linebreak-style */
 function estimateFutureCases(periodType, currentInfectedCases) {
-    let factor = Math.floor(28 / 3);
-    switch (periodType) {
-        // 7 days * # 28 weeks
-        case 'weeks':
-            factor = Math.floor((7 * 28) / 3);
-            break;
-        case 'months':
-            // 30 days * 28 months
-            factor = Math.floor((30 * 28) / 3);
-            break;
-    }
+  let factor;
+  switch (periodType) {
+    // 7 days * # 28 weeks
+    case 'weeks':
+      factor = Math.floor((7 * 28) / 3);
+      break;
+    case 'months':
+      // 30 days * 28 months
+      factor = Math.floor((30 * 28) / 3);
+      break;
+    default:
+      factor = Math.floor(28 / 3);
+      break;
+  }
 
-    return currentInfectedCases * (2 ** factor);
+  return currentInfectedCases * 2 ** factor;
 }
+
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
+  // reported cases
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  // Estimate cases for the next 28 days on impact
+  impact.infectionsByRequestedTime = estimateFutureCases(
+    data.periodType,
+    impact.currentlyInfected
+  );
+  // Estimate cases for the next 28 days on severe impact
+  severeImpact.infectionsByRequestedTime = estimateFutureCases(
+    data.periodType,
+    severeImpact.currentlyInfected
+  );
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
